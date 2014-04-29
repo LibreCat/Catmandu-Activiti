e40eff358a43e8cc6f3dc875e3324d859a883e98
Nicolas Franck <nicolas.franck@ugent.be>
2014-04-29 11:32:33 +0200

    documentation

---
+++ lib/Catmandu/Activiti.pm
@@ -0,0 +1,3 @@
+package Catmandu::Activiti;
+our $VERSION = "0.1";
+1;
---
+++ lib/Catmandu/Importer/Activiti/RuntimeProcessInstance.pm
@@ -0,0 +1,107 @@
+package Catmandu::Importer::Activiti::RuntimeProcessInstance;
+use Catmandu::Sane;
+use Catmandu::Util qw(:is :check :array);
+use Activiti::Rest;
+use Moo;
+
+with 'Catmandu::Importer';
+
+has url => (
+  is => 'ro',
+  isa => sub { check_string($_[0]); },
+  required => 1
+);
+has include_process_variables => (
+  is => 'ro',
+  isa => sub { array_includes([qw(true false)],$_[0]) or die("must be true of false"); },
+  lazy => 1,
+  default => sub { "false"; }
+);
+has _activiti => (
+  is => 'ro',
+  lazy => 1,
+  builder => '_build_activiti'
+);
+sub _build_activiti {
+  my $self = $_[0];
+  Activiti::Rest->new(url => $self->url);
+}
+
+sub generator {
+  my $self = $_[0];
+  sub {
+
+    state $start = 0;
+    state $size = 100;
+    state $total;
+    state $results = [];
+
+    unless(@$results){
+
+      if(defined $total){
+        return if $start >= $total;
+      }
+
+      my $res = $self->_activiti->process_instances(
+        start => $start,
+        size => $size,
+        includeProcessVariables => $self->include_process_variables()
+      )->parsed_content;
+
+      $total = $res->{total};
+      return unless @{ $res->{data} };
+
+      $results = $res->{data};
+      
+      $start += $size;
+    }
+
+    shift @$results;
+  };
+}
+
+=head1 NAME
+
+Catmandu::Importer::Activiti::RuntimeProcessInstance - Package that imports runtime process instances from Activiti
+
+=head1 SYNOPSIS
+
+    use Catmandu::Importer::Activiti::RuntimeProcessInstance;
+
+    my $importer = Catmandu::Importer::Activiti::RuntimeProcessInstance->new(
+      url => 'http://user:password@localhost:8080/activiti-rest/service'
+    );
+
+    my $n = $importer->each(sub {
+        my $hashref = $_[0];
+        # ...
+    });
+
+=head1 METHODS
+
+=head2 new()
+
+Create a new importer
+
+Arguments:
+
+  include_process_variables   "true"|"false"  (default: "false")
+
+=head2 each(&callback)
+
+=head2 ...
+
+Every Catmandu::Importer is a Catmandu::Iterable all its methods are inherited. The
+Catmandu::Importer::Activiti::RuntimeProcessInstance methods are not idempotent: Activiti feeds can only be read once.
+
+=head1 SEE ALSO
+
+L<Catmandu::Iterable>
+
+=head1 AUTHOR
+
+Nicolas Franck C<< Nicolas Franck at UGent be >>
+
+=cut
+
+1;
